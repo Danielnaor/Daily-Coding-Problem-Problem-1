66cdeb33b73fa354fd7278aa5ac22747230eb33a
Danielnaor <dnaorcode@gmail.com>
2023-05-23 10:48:56 -0500

    commit 05.23.2023

--- src/main/java/com/mycompany/Daily_Coding_Problem_Problem1/Daily_Coding_Problem_Problem1.java
+++ src/main/java/com/mycompany/Daily_Coding_Problem_Problem1/Daily_Coding_Problem_Problem1.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.Daily_Coding_Problem_Problem1;
 
 /**
@@ -11,16 +10,16 @@ package com.mycompany.Daily_Coding_Problem_Problem1;
  */
 import java.util.Scanner;
 import java.util.Arrays;
-public class Daily_Coding_Problem_Problem1 {
-    public static void main(String[] args) {
-       
-        Scanner input = new Scanner(System.in);
-        System.out.print("what would be the list? (example: 10,15,3,7) :" );
-        
-        String[] listArrystring = input.nextLine().split(",");
-        
 
-    
+public class Daily_Coding_Problem_Problem1 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("what would be the list? (example: 10,15,3,7) :");
+
+        String[] listArrystring = input.nextLine().split(",");
+
         int[] list = new int[listArrystring.length];
 
         // Loop trough the String array and convet each value to int and adds int to the int[] list 
@@ -33,20 +32,20 @@ public class Daily_Coding_Problem_Problem1 {
 
         int num1;
         int num2;
-        for(int i = 0; i < list.length; i++){
+        for (int i = 0; i < list.length; i++) {
             num1 = list[i];
-                for(int i2 = 0; i2 < list.length; i2++){
-                    num2 = list[i2];
-                    if((num1+num2) == k){
-                        System.out.println(num1 + " + " + num2 + " = " + k);
-                        return;
+            for (int i2 = 0; i2 < list.length; i2++) {
+                num2 = list[i2];
+                if ((num1 + num2) == k) {
+                    System.out.println(num1 + " + " + num2 + " = " + k);
+                    return;
 
-                    }
-                           
                 }
-                
+
+            }
+
         }
-                System.out.print(" there is no combination of any two numbers from the list add up to  " + k);
+        System.out.print(" there is no combination of any two numbers from the list add up to  " + k);
 
     }
 }
